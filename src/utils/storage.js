ba447911a8a9f24061f43e82eb5cac5d6ea54b70
tonxxd <2827496+tonxxd@users.noreply.github.com>
2020-01-16 12:22:37 +0100

    fix cross domain state consistency

---
+++ src/utils/storage.js
@@ -0,0 +1,29 @@
+
+
+class Storage {
+    static async get(key){
+        return new Promise(res => {
+            chrome.storage.sync.get([key], result => {
+                res(result[key]);  
+            });
+        })
+    }
+
+    static getAll(){
+        return new Promise(res => {
+            chrome.storage.sync.get(null, result => {
+                res(result);  
+            });    
+        })
+    }
+    static async set(obj, value = false){
+
+        return new Promise(res => {
+            chrome.storage.sync.set(typeof obj === 'string' ? {[obj]:value} : obj, () => {
+                res()
+            })
+        })
+    }
+}
+
+export default Storage
