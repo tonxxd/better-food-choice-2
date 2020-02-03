3d59ea86dabadbb58ba635c4f330a9882f34204b
tonxxd <2827496+tonxxd@users.noreply.github.com>
2020-02-03 17:32:40 +0100

    updates

---
+++ src/BetterChoices/taskDesc.js
@@ -0,0 +1,50 @@
+export default `
+
+<h1>Task description Nutri-score study</h1>
+<p><span style="font-weight: bold">Version</span>: 1</p>
+<p><span style="font-weight: bold">Author</span>: LM</p>
+<p><span style="font-weight: bold">Changes</span>: - </p>
+
+<p><span style="font-weight: bold">Einleitung:</span></p>
+<p>Liebe Teilnehmerin, lieber Teilnehmer,</p>
+
+<p>für die folgenden Aufgabe sollen Sie verschiedene Lebensmittel in einem Online-Supermarkt einkaufen. Der Supermarkt ist wie ein ganz normaler Onlineshop aufgebaut. Wenn Sie ein Produkt gefunden haben, das sie Ihrem Warenkorb hinzufügen möchten, können Sie dies durch einen Klick auf die Schaltfläche „Auf die Einkaufsliste“ in der Detailansicht des Produktes tun. In die Detailansicht wechseln Sie, indem Sie auf das Bild des Produktes oder seinen Namen klicken.
+Die Detailansicht liefert folgende Informationen zu dem Produkt:
+<li>Nährwerte (100g und eine Portion)</li>
+<li>fünfstufige Farb- und Buchstabenskala, die einen Überblick über die Nährwertqualität eines Produktes liefert (A-E); bezieht Energiegehalt, Zucker, gesättigte Fettsäuren, Natrium, Proteine, Ballaststoffe und Obst-, Gemüse- bzw. Nussanteil mit ein</li>
+<li>Zutaten des Produktes</li>
+<li>Allgemeine Produktinformationen (Kühlung, Verpackungsart, Herkunft etc.)</li> </p>
+
+<hr>
+
+<p>Für die folgende Aufgabe möchten wir Sie bitten sich vorzustellen, dass Sie für sich und zwei Freunde von Ihnen ein Abendessen planen. Dabei haben Sie sich dazu entschlossen, die notwendigen Lebensmittel in einem Onlineshop einzukaufen. 
+Für ein Budget von insgesamt CHF 100/ EUR 55 möchten Sie für sich und ihre Gäste eine Vorspeise und ein Hauptgericht zubereiten. Außerdem möchten Sie nicht-alkoholische Getränke bereitstellen.
+Gehen Sie bitte davon aus, dass Ihre Küche mit den grundlegenden Gewürzen und Öl ausgestattet ist. Sie müssen demnach nur die Hauptzutaten für die Gerichte einkaufen. </p>
+
+<p>Die folgende Liste kann Ihnen als grobe Richtlinie für Ihr Menü dienen. Sie müssen sich nicht strikt an die Vorschläge halten und können alle möglichen Lebensmittel auswählen.</p>
+
+<li>Vorspeisenplatte:
+<li style="padding-left:20px">Salat/Suppe mit Gemüse oder Fleisch</li>
+<li style="padding-left:20px">Brot</li>
+<li style="padding-left:20px">Käse/Würste</li>
+</li>
+
+
+<li>Hauptgericht: 
+<li style="padding-left:20px">Fleisch, Fisch oder Gemüse</li>
+<li style="padding-left:20px">Beilagen (e.g. Nudeln, Reis, Kartoffeln oder anderes Gemüse)</li>
+<li style="padding-left:20px">Saucen</li>
+</li>
+
+<li>Getränke:
+
+<li style="padding-left:20px">Wasser</li>
+<li style="padding-left:20px">Soft-drinks</li>
+<li style="padding-left:20px">Säfte</li>
+</li>
+
+ 
+Nehmen Sie sich bitte so viel Zeit wie Sie möchten, um die verschiedenen Produkte und Kategorien zu erkunden. Wenn Sie ein Produkt kaufen möchten, fügen Sie es bitte Ihrem Warenkorb hinzu. Sobald Sie alle, für Ihr Menü notwendigen, Produkte hinzugefügt haben, fahren Sie bitte zur Kasse fort. 
+
+
+`
