ba10ba6e34e793cf40e7961e18c332733b1c8a6e
tonxxd <2827496+tonxxd@users.noreply.github.com>
2020-03-05 21:35:41 +0100

    fix

--- src/BetterChoices/CurrencyConverter/index.js
+++ src/BetterChoices/CurrencyConverter/index.js
@@ -9,7 +9,8 @@ export /**
  * @returns
  */
 const convertPrice = (price, category) => {
-
+    console.log(price, category)
+    price = parseFloat(price.toString().replace("€",'').replace('.-','').replace('-','').replace("chf",'').replace('CHF','').replace('EUR','').replace('eur','') || 0)
     return (Math.round(price*100*(ratios[category]|| 0.542107325))/100).toFixed(2);
 
 }
